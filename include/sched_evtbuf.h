d868eee8915813d76c61ac987d73c6a0c855dfa9
Keda Huang <huangkeda88@163.com>
2017-03-10 21:10:31 +0800

    Core: 添加'事件缓存管理'
    1. 添加配置项SCHED_EVTBUF_QUEUE_EN
    2. 添加事件缓存管理的相关函数定义

---
+++ include/sched_evtbuf.h
@@ -0,0 +1,27 @@
+/*******************************************************************************
+* 文 件 名: sched_evtbuf.h
+* 创 建 者: Keda Huang
+* 版    本: V1.0
+* 创建日期: 2017-02-22
+* 文件说明: 事件驱动调度器的事件缓存管理
+*******************************************************************************/
+
+#ifndef __SCHED_EVTBUF_H
+#define __SCHED_EVTBUF_H
+
+/* 头文件 --------------------------------------------------------------------*/
+#include "sched_proto.h"
+
+/* 事件缓存类型 --------------------------------------------------------------*/
+struct sched_evtbuf;
+typedef struct sched_evtbuf sSchedEvtbuf;
+
+/* 事件缓存管理 --------------------------------------------------------------*/
+sSchedEvtbuf *schedEvtbufCreate(EvtPos_t len);
+void schedEvtbufDelete(sSchedEvtbuf *evtbuf);
+eSchedBool schedEvtbufSend(sSchedEvtbuf *evtbuf, const SchedEvent_t *tx_evt);
+eSchedBool schedEvtbufSendToFront(sSchedEvtbuf *evtbuf, const SchedEvent_t *tx_evt);
+eSchedBool schedEvtbufReceive(sSchedEvtbuf *evtbuf, SchedEvent_t *rx_evt);
+eSchedBool schedEvtbufIsEmpty(sSchedEvtbuf *evtbuf);
+
+#endif  /* __SCHED_EVTBUF_H */
