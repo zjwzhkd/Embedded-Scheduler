e88dbbf988912c4c2e6b1c0f5930d969f3822050
Keda Huang <huangkeda88@163.com>
2017-03-17 22:14:05 +0800

    Core: 实现'调度器核心函数'
    1. 实现调度器初始化函数schedInit()
    2. 实现调度器启动函数schedStart()

--- core/sched_core.c
+++ core/sched_core.c
@@ -7,9 +7,43 @@
 *******************************************************************************/
 
 #include "sched_core.h"
+#include "sched_task.h"
+#include "sched_tick.h"
 /*******************************************************************************
 
                                     全局变量
 
 *******************************************************************************/
 eSchedCoreState schedCoreCurrentState = SCHED_CORE_UNINITIALIZED;
+
+/*******************************************************************************
+
+                                    核心管理
+
+*******************************************************************************/
+/* 调度器初始化 */
+void schedInit(void)
+{
+    schedPortInit();
+    schedTickInit();
+    schedTaskInit();
+    schedCoreCurrentState = SCHED_CORE_STOP;
+}
+
+/* 启动调度器 */
+void schedStart(void)
+{
+eSchedBool exec;
+
+    schedCoreCurrentState = SCHED_CORE_RUNNING;
+    schedTaskInitialiseAll();
+
+    for (;;)
+    {
+        exec = schedTaskExecute();
+        if ( exec == SCHED_FALSE )
+        {
+
+        }
+    }
+}
